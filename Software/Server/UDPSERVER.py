ddba196611c2408d4d975e5c3e52238de0ee5ef7
NorthernDIY <70189433+NorthernDIY@users.noreply.github.com>
2022-05-20 17:36:27 -0500

    Update UDPSERVER.py
    UDP Server development.  Adding initial packet handling/identification

--- Software/Server/UDPSERVER.py
+++ Software/Server/UDPSERVER.py
@@ -1,6 +1,38 @@
 #Based off https://pythontic.com/modules/socket/udp-client-server-example
 
-import socket, msgpack, pprint
+import socket, msgpack, pprint, sqlite3
+from enum import Enum
+class msgType(Enum):
+    JUNK= 0
+    INIT = 1
+    HALL = 2
+    ACCEL = 3
+
+def unpack(UDP_packet):
+    message = NULL
+    try:
+        message = msgpack.loads(UDP_packet)
+    except:
+        print("Failed to decode MSGPACK")
+        #udp send 0 (Failed)
+    else:
+        #udp send 1 (good)
+        print("MSGPACK Decoded")
+    finally:
+        return message
+
+def getMsgType(message):
+    if message.has_key("Hello"):
+        return msgType.INIT
+    elif message.has_key("TH"):
+        return msgType.HALL
+    elif message.has_key("TA"):
+        return msgType.ACCEL
+    else:
+        print("Bad Packet Type!")
+        return msgType.JUNK
+
+
 localIP     = "127.0.0.1"# Localhost for now
 localPort   = 20002
 bufferSize  = 20000
@@ -18,14 +50,26 @@ print("UDP server up and listening")
 # Listen for incoming datagrams
 while(True):
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
+    Packet = bytesAddressPair[0]
     address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    unpacked = msgpack.loads(message)
-    clientIP  = "Client IP Address:{}".format(address) 
-    print(clientMsg)
-    print(clientIP)
-    pprint.pprint(unpacked)
+    #clientMsg = "Message from Client:{}".format(message)
+    unpacked = unpack(Packet)
+    #clientIP  = "Client IP Address:{}".format(address) 
+    #print(clientMsg)
+    #print(clientIP)
+    mType = getMsgType(unpacked)
+    if (mType==msgType.INIT):
+        print("New Session")
+        pprint.pprint(unpacked)
+    elif (mType==msgType.HALL):
+        print("H Data Rx'd")
+        pprint.pprint(unpacked)
+    elif (mType==msgType.ACCEL):
+        print("A Data Rx'd")
+        pprint.pprint(unpacked)
+    else:
+        print("Bad Packet!")
 
     # Sending a reply to client
     UDPServerSocket.sendto(bytesToSend, address)
+
