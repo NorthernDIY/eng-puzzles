2d0cffac4f28df22b1a7dbc4fd3f1674725ba90c
GoTeam15 <70189433+NorthernDIY@users.noreply.github.com>
2022-06-02 18:22:13 -0500

    Continued Server Development
    SERVER: Now correctly determines message type.
    SQLITE3 initial setup and reset functions added.
    
    PacketSimulator:  Minor changes, UDP timeout to wait for responses, exits on timeout.

--- Software/PacketSimulator/ESP32JSONPacketSimulator.py
+++ Software/PacketSimulator/ESP32JSONPacketSimulator.py
@@ -14,7 +14,7 @@ bufferSize = 2000
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
+UDPClientSocket.settimeout(4)
 
 start_time = datetime.now()
 
@@ -62,83 +62,94 @@ msgFromClient = msgpack.dumps(ResetDictionary)
 bytesToSend = msgFromClient
 print("*MSGPACK* Sending: %d bytes...\n" % (len(bytesToSend)))
 print("(*JSON* Would be: %d bytes)" % (len(json.dumps(ResetDictionary))))
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-SessionID = UDPClientSocket.recvfrom(bufferSize)
-print(SessionID)
+try:
+	UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+	SessionIDResponse = UDPClientSocket.recvfrom(bufferSize)[0]
+	print(SessionIDResponse)
+	SessionID = msgpack.loads(SessionIDResponse)["ID"]
+	print("Session ID: %d" %(SessionID))
+except socket.timeout:
+	print("TIMEOUT! No response from Server, Exiting.")
+	UDPClientSocket.close()
+	sys.exit()
 # sys.exit()
 runTime = millis()
 halltime = runTime  # tracks last packet send time
 acceltime = runTime  # tracks last packet send time
 while timestamp_hall < 60*60 / 50e-3:  # 1 Hour of 50mSec Packets
+	try:
+		#print(runTime - halltime)
 
-    #print(runTime - halltime)
+		# print((runTime-halltime)>reportIntervalHall)
+		# Hall Sensor Packet
+		if ((runTime - halltime) > (reportIntervalHall)):
+			halltime = runTime
+			HallDictionary['TH'] = timestamp_hall
+			for index in range(0, hallBlockSize):
+				KeyValx = "x" + str(index)
+				KeyValy = "y" + str(index)
+				#KeyValz = "Hz" + str(index)
+				HallDictionary[KeyValx] = []
+				HallDictionary[KeyValy] = []
+				#JsonHDictionary[KeyValz] = []
 
-    # print((runTime-halltime)>reportIntervalHall)
-    # Hall Sensor Packet
-    if ((runTime - halltime) > (reportIntervalHall)):
-        halltime = runTime
-        HallDictionary['TH'] = timestamp_hall
-        for index in range(0, hallBlockSize):
-            KeyValx = "x" + str(index)
-            KeyValy = "y" + str(index)
-            #KeyValz = "Hz" + str(index)
-            HallDictionary[KeyValx] = []
-            HallDictionary[KeyValy] = []
-            #JsonHDictionary[KeyValz] = []
+				for x in range(0, NumSensors):
+					HallDictionary[KeyValx].append(random.randrange(-2047, 2047))
+					HallDictionary[KeyValy].append(random.randrange(-2047, 2047))
+					# JsonHDictionary[KeyValx].append(-2046.012)
+					# JsonHDictionary[KeyValy].append(-2046.012)
+					#JsonHDictionary[KeyValz].append(random.randrange(-2047, 2047))
 
-            for x in range(0, NumSensors):
-                HallDictionary[KeyValx].append(random.randrange(-2047, 2047))
-                HallDictionary[KeyValy].append(random.randrange(-2047, 2047))
-                # JsonHDictionary[KeyValx].append(-2046.012)
-                # JsonHDictionary[KeyValy].append(-2046.012)
-                #JsonHDictionary[KeyValz].append(random.randrange(-2047, 2047))
+			if (reportIntervalHall > 500):
+				print("Hall Packet: ", end="")
+				# print(JsonHDictionary)
+				# print()
 
-        if (reportIntervalHall > 500):
-            print("Hall Packet: ", end="")
-            # print(JsonHDictionary)
-            # print()
+			jsonHObject = json.dumps(HallDictionary)
+			#r = requests.post(Hall_url, json = jsonHObject, headers=jsonHeaders)
+			msgFromClientH = msgpack.dumps(HallDictionary)
+			bytesToSend = msgFromClientH
+			print("H  *MSGPACK* Sending: %d bytes..." % (len(bytesToSend)), end="")
+			print("(*JSON* Would be: %d bytes)" %
+				  (len(json.dumps(HallDictionary))))
+			UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+			timestamp_hall = timestamp_hall + (hallBlockSize*sampleIntervalHall)
+			HallDictionary = {}
 
-        jsonHObject = json.dumps(HallDictionary)
-        #r = requests.post(Hall_url, json = jsonHObject, headers=jsonHeaders)
-        msgFromClientH = msgpack.dumps(HallDictionary)
-        bytesToSend = msgFromClientH
-        print("H  *MSGPACK* Sending: %d bytes..." % (len(bytesToSend)), end="")
-        print("(*JSON* Would be: %d bytes)" %
-              (len(json.dumps(HallDictionary))))
-        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-        timestamp_hall = timestamp_hall + (hallBlockSize*sampleIntervalHall)
-        HallDictionary = {}
+		# Accel Packet
+		if ((runTime - acceltime)>(reportIntervalAccel)):
+			# if (0):
+			acceltime = runTime
 
-    # Accel Packet
-    if ((runTime - acceltime)>(reportIntervalAccel)):
-        # if (0):
-        acceltime = runTime
+			HallDictionary["TA"] = timestamp_accel
+			for x in range(0, accelBlockSize):
+				AccelDictionary["x"].append(
+					random.randrange(-2047, 2047))
+				AccelDictionary["y"].append(
+					random.randrange(-2047, 2047))
+				AccelDictionary["z"].append(
+					random.randrange(-2047, 2047))
 
-        HallDictionary["TA"] = timestamp_accel
-        for x in range(0, accelBlockSize):
-            AccelDictionary["x"].append(
-                random.randrange(-2047, 2047))
-            AccelDictionary["y"].append(
-                random.randrange(-2047, 2047))
-            AccelDictionary["z"].append(
-                random.randrange(-2047, 2047))
+			"""if (reportIntervalAccel > 500):
+				print("Accel Packet: ", end="")
+				print(AccelDictionary)
+				print()
+			"""
+			jsonAObject = json.dumps(AccelDictionary)
+			#r = requests.post(Accel_url, json=jsonAObject, headers=jsonHeaders)
+			msgFromClientA = msgpack.dumps(AccelDictionary)
+			bytesToSend = msgFromClientA
+			print("A  *MSGPACK* Sending: %d bytes..." % (len(bytesToSend)), end="")
+			print("(*JSON* Would be: %d bytes)" %
+				  (len(json.dumps(AccelDictionary))))
+			UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+			timestamp_accel = timestamp_accel + \
+				(accelBlockSize*sampleIntervalAccel)
+			AccelDictionary = {"TA": timestamp_accel, "x": [], "y": [], "z": []}
 
-        """if (reportIntervalAccel > 500):
-            print("Accel Packet: ", end="")
-            print(AccelDictionary)
-            print()
-        """
-        jsonAObject = json.dumps(AccelDictionary)
-        #r = requests.post(Accel_url, json=jsonAObject, headers=jsonHeaders)
-        msgFromClientA = msgpack.dumps(AccelDictionary)
-        bytesToSend = msgFromClientA
-        print("A  *MSGPACK* Sending: %d bytes..." % (len(bytesToSend)), end="")
-        print("(*JSON* Would be: %d bytes)" %
-              (len(json.dumps(AccelDictionary))))
-        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-        timestamp_accel = timestamp_accel + \
-            (accelBlockSize*sampleIntervalAccel)
-        AccelDictionary = {"TA": timestamp_accel, "x": [], "y": [], "z": []}
-
-    # Update current time for next loop
-    runTime = millis()
+		# Update current time for next loop
+		runTime = millis()
+	except socket.timeout:
+		print("TIMEOUT! No response from Server, Exiting.")
+		UDPClientSocket.close()
+		sys.exit()
--- Software/Server/UDPSERVER.py
+++ Software/Server/UDPSERVER.py
@@ -1,7 +1,31 @@
 #Based off https://pythontic.com/modules/socket/udp-client-server-example
 
-import socket, msgpack, pprint, sqlite3
+import socket, msgpack, pprint, sqlite3, sys, os
+from time import sleep
+from colorama import Fore,Back,Style
 from enum import Enum
+
+#Variables
+localIP     = "127.0.0.1"# Localhost for now
+localPort   = 20002
+bufferSize  = 20000
+
+#SQL Things
+sqlConn = ''
+cursor = ''
+db_fName = 'SeriousGamesRaw.db'
+
+#Session Things
+sessionID = 0
+
+HBSz = 0 #Hall Sensor Block Size
+HT = 0 #Hall Sensor Time Stamp (last entry)
+HSensors = 24 #Number of Hall sensors
+
+ABSz = 0 #Accelerometer Block Size
+AT = 0 #Accelerometer Time Stamp (last entry)
+
+
 class msgType(Enum):
     JUNK= 0
     INIT = 1
@@ -9,7 +33,7 @@ class msgType(Enum):
     ACCEL = 3
 
 def unpack(UDP_packet):
-    message = NULL
+    message = None
     try:
         message = msgpack.loads(UDP_packet)
     except:
@@ -22,24 +46,49 @@ def unpack(UDP_packet):
         return message
 
 def getMsgType(message):
-    if message.has_key("Hello"):
+    if message.__contains__("Hello"):
         return msgType.INIT
-    elif message.has_key("TH"):
+    elif message.__contains__("TH"):
         return msgType.HALL
-    elif message.has_key("TA"):
+    elif message.__contains__("TA"):
         return msgType.ACCEL
     else:
         print("Bad Packet Type!")
         return msgType.JUNK
 
-
-localIP     = "127.0.0.1"# Localhost for now
-localPort   = 20002
-bufferSize  = 20000
+def setupSQLdb(fName):
+    global sqlConn, cursor
+    sqlConn = sqlite3.connect(fName, timeout = 10)
+    cursor = sqlConn.cursor()
+    createParamTable = "CREATE TABLE Params(sessionID INTEGER PRIMARY KEY, datetime TEXT);"
+    createHallTable = "CREATE TABLE Hall(senseNum INTEGER, time INTEGER, x INTEGER, y INTEGER, PRIMARY KEY(senseNum, time));"
+    createAccelTable = "CREATE TABLE Accel(time integer PRIMARY KEY, x INTEGER, y INTEGER, z INTEGER);"
+    cursor.execute(createHallTable)
+    cursor.execute(createAccelTable)
+    sqlConn.commit()
+    
  
-msgFromServer       = "Hello UDP Client"
-bytesToSend         = str.encode(msgFromServer)
+#msgFromServer       = "Hello UDP Client"
+#bytesToSend         = str.encode(msgFromServer)
 
+args = sys.argv
+if len(args)==2:
+    if args[1]=="--resetdb":
+        if os.path.exists(db_fName):
+            print("ResetDB:  Deleting old DB file")
+            os.remove(db_fName)
+            sleep(1)
+        else:
+            print("ResetDB:  No DB file to delete (Not an error)")
+        
+        #Regenerate new sql db file with tables/headers
+        setupSQLdb(db_fName)
+        print("Done, Exiting...")
+        sqlConn.close()
+        sys.exit()
+    else:
+        print("Invalid arguments, only option is --resetdb")
+        sys.exit()
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
@@ -53,23 +102,29 @@ while(True):
     Packet = bytesAddressPair[0]
     address = bytesAddressPair[1]
     #clientMsg = "Message from Client:{}".format(message)
+    #pprint.pprint(Packet)
+    
     unpacked = unpack(Packet)
-    #clientIP  = "Client IP Address:{}".format(address) 
-    #print(clientMsg)
-    #print(clientIP)
-    mType = getMsgType(unpacked)
-    if (mType==msgType.INIT):
-        print("New Session")
-        pprint.pprint(unpacked)
-    elif (mType==msgType.HALL):
-        print("H Data Rx'd")
-        pprint.pprint(unpacked)
-    elif (mType==msgType.ACCEL):
-        print("A Data Rx'd")
-        pprint.pprint(unpacked)
-    else:
-        print("Bad Packet!")
-
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+    if type(unpacked) != None:
+        mType = getMsgType(unpacked)
+        #clientIP  = "Client IP Address:{}".format(address) 
+		#print(clientMsg)
+		#print(clientIP)
+        if (mType==msgType.INIT):
+            print("New Session")
+            sessionID +=1;
+            SIDresponse = {'ID':sessionID}
+            msgToClient = msgpack.dumps(SIDresponse)
+            UDPServerSocket.sendto(msgToClient, address)
+            #pprint.pprint(unpacked)
+        elif (mType==msgType.HALL):
+            print("H Data Rx'd")
+            #pprint.pprint(unpacked)
+        elif (mType==msgType.ACCEL):
+            print("A Data Rx'd")
+        #pprint.pprint(unpacked)
+        else:
+            print("Bad Packet!")
+        # Sending a reply to client
+        #UDPServerSocket.sendto(bytesToSend, address)
 
